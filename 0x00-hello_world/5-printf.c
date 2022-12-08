60891c8647bc0e9e7635e4b946b2f244722b0136
lowryel <eugeneagbaglo@gmail.com>
2022-12-08 15:58:28 -0800

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Entry - point
+ *
+ * Return exact output with printf function
+ *
+ * Return: Always 0 (Success)
+ * /
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
