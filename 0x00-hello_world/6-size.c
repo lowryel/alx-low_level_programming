a0b5d8a8bd2091c2de536cbc4582ccd4f428d550
lowryel <eugeneagbaglo@gmail.com>
2022-12-09 00:20:06 -0800

    use write()to print out texts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main: print the size of various types
+ * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled
- * and run on
+ * Description: prints the size of various types on the
+ * computer it is compiled and run on
  *
  * Return: Allways 0 (Success)
  */
