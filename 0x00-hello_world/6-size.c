da83fe1deca5cb7b8a8a55dbd2a924743e6ea7c6
lowryel <eugeneagbaglo@gmail.com>
2022-12-08 23:58:48 -0800

    get size of c data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print the size of various types
-*
-* Description: prints the size of various types on the computer it is compiled
-* and run on
-* Return: Allways 0 (Success)
-*/
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ * Return: Allways 0 (Success)
+ */
 
 int main(void)
 {
@@ -15,6 +15,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	
 	return (0);	
 }
