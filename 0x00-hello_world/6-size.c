042673939a296faef51e1952c8f93ddfab80432a
lowryel <eugeneagbaglo@gmail.com>
2022-12-08 16:12:26 -0800

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - print the size of various types
- *
- * Description: prints the size of various types on the computer it is compiled
- * and run on
- *
- * Return: Allways 0 (Success)
- */
+* main - print the size of various types
+*
+* Description: prints the size of various types on the computer it is compiled
+* and run on
+* Return: Allways 0 (Success)
+*/
 
 int main(void)
 {
