b91234911cc0bb8ff4461e65d18c264c549b909f
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 15:42:28 -0800

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -lowercase base 16 characters
+ *
+ * Description: return base 16 characters in lowercase
+ *
+ * Retrun: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 48)
+	{
+		if (a < 10)
+
+			putchar(a + '0');
+		else if (a > 41)
+			putchar(a - 10 +'A');
+		a++;
+	}
+	putchar(10);
+
+	return (0);
+}
