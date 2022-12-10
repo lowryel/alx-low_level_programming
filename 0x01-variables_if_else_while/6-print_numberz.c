2e707410fe8c9de6d884dc2fc2d4d5949f1cec03
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 06:24:32 -0800

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - numbers less tha 10
+ *
+ * Description: numbers less than 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	int i = 20;
+
+	while (i < 30)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
