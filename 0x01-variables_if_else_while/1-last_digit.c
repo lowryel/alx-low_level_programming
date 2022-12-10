90085a988716d2db5290ac68447302ce282a4347
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 02:37:41 -0800

    return last digit of a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,6 @@
  *
  * Return: Always 0 (success)
  */
- 
 
 int main(void)
 
@@ -24,7 +23,7 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	last = n%10;
+	last = n % 10;
 	if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last == 0)
