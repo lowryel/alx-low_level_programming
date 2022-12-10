53a00e43e65eb70700ffc2c4b19c632afe8efa15
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 05:28:04 -0800

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdiio.h>
+
+/**
+ * main - exclude (e and q)
+ *
+ * Description: lowercase alphabets excluding e and q
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+		if (a != 101 && a != 103)
+		{
+			putchar(i);
+		}
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
