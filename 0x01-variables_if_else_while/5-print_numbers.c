7e7c5b6736c8275f8c977469bb5bf845afc05ebe
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 06:00:45 -0800

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - base ten (10) numbers
+ *
+ * Description: return base 10 single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		printf("%a\n", a);
+	}
+	return (0);
+}
