8855c61f53e0fe4e0d516aa1201073719e0faf6c
lowryel <eugeneagbaglo@gmail.com>
2022-12-11 04:33:06 -0800

    tripple digit numbers from base 10 numerals

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - print tripple digit numbers
+ *
+ * Description: print tripple digit numbers with commas from base ten numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, a, c;
+
+	i = 48;
+	a = 48;
+	c = 48;
+
+	while (i < 58)
+	{
+		a = i + 1;
+
+		while (a < 58)
+		{
+			while (c < 58)
+			{
+			
+				putchar(i);
+				putchar(a);
+				putchar(c);
+
+				if (i < 55 || a < 56 || c < 57)
+
+				{
+					putchar(44);
+					putchar(32);
+				}
+				c++;
+			}
+			a++
+		}
+		i++
+
+	}
+	putchar(10);
+
+	return (0);
+}
