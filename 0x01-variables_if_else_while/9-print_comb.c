4e53207afccb8dabfaaa9230730ea5d348ee6df2
lowryel <eugeneagbaglo@gmail.com>
2022-12-10 16:20:55 -0800

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - possible combination of single digit numbers
+ *
+ * Description: return the possible combination of single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	int a = 48;
+
+	while (a < 58)
+
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		a++;
+	}
+
+	putchar (10);
+
+	return (0);	
+}
