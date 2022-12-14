28e1575d4ff07b29c843a16019878343476c2d22
lowryel <eugeneagbaglo@gmail.com>
2022-12-14 14:20:59 -0800

    print output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - return an output
+ *
+ * Description: function to return an output with a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	printf("_putchar");
+
+	return (0);
+}
