e0c6d77bcb272c04dc0e5a42f48a878813cae959
lowryel <eugeneagbaglo@gmail.com>
2022-12-14 15:32:50 -0800

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - lower case alphabets
+ *
+ * Description: return lower case alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a < ='z'; a++)
+		_putchar(a);
+
+
+	_putchar('\n');
+
+	return (0);
+}
+
