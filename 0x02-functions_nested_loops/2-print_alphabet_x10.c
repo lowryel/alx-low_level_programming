a64a6c26b02b143c5fbc02af02795275f5367e9f
lowryel <eugeneagbaglo@gmail.com>
2022-12-14 15:53:20 -0800

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10x alphabets
+ *
+ * Description: return the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet_count = 0;
+
+	while (alphabet_count < 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+
+			_putchar(ch);
+			ch++;
+		}
+
+		alphabet_count++
+		_putchar('\n');
+	}
+}
