87b7f53c34b49a98ef84c384628bad038dd19bb4
lowryel <eugeneagbaglo@gmail.com>
2022-12-15 01:03:51 -0800

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,6 @@
  */
 
 void times_table(void)
-
 {
 
 	int r, c, d;
@@ -28,34 +27,29 @@ void times_table(void)
 		{
 			d = (r * c);
 
-													if ((d / 10) > 0)
+		if ((d / 10) > 0)
+		
+		{
+			_putchar((d / 10) + '0');
+		}
 
-			{
-				_putchar((d / 10) + '0');
+		else
 
-													}
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
+		if (c < 9)
 
-			else
+		{
+			_putchar(',');
 
-			{
+			_putchar(' ');
+		}
 
-				_putchar(' ');
-													}
-														_putchar((d % 10) + '0');
-
-													if (c < 9)
-
-			{
-				_putchar(',');
-
-				_putchar(' ');
-
-													}
-
-												}
+	}
 
 		_putchar('\n');
-
 	}
 
 }
