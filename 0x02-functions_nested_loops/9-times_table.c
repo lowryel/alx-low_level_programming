9b2033ac4f4c366dcb1e59a97887821332b05ec7
lowryel <eugeneagbaglo@gmail.com>
2022-12-15 00:52:54 -0800

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
+ * times_table -  a function that prints the 9 times table, starting with 0
  *
- *  * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
  *
- *   * rone = row, cone = column, d = digits of current result
+ * Return: times table
  *
- *    * Return: times table
- *
- *     * add extra space past single digit
- *
- *      */
+ * add extra space past single digit
+ */
 
 void times_table(void)
 
