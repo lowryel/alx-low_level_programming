cbcc321951d615da9e89daa90d564da58bf05650
lowryel <eugeneagbaglo@gmail.com>
2022-12-15 02:58:53 -0800

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -23,7 +23,6 @@ int main(void)
 
 	for (i = 1; i < 91; i++)
 	{
-
 		printf(", %lu", k);
 
 		k = k + j;
@@ -31,7 +30,6 @@ int main(void)
 		j = k - j;
 
 	}
-
 	j1 = j / 1000000000;
 
 	j2 = j % 1000000000;
@@ -44,7 +42,6 @@ int main(void)
 
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
-
 		printf("%lu", k2 % 1000000000);
 
 		k1 = k1 + j1;
@@ -57,6 +54,5 @@ int main(void)
 
 	}
 		printf("\n");
-
 		return (0);
 }
