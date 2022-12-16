aa74f75e7d3187e08d0f787b8cf80f1f4376457e
lowryel <eugeneagbaglo@gmail.com>
2022-12-16 02:10:44 -0800

    more loops and function structures

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - upper case letters
+ *
+ * @c: parameter
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
