471c96e1687fd95d21bcc93b6ca45b37b7a91fea
lowryel <eugeneagbaglo@gmail.com>
2022-12-15 23:39:16 -0800

    a

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,20 +3,14 @@
 /**
  * main - Debugging.
  *
- * Description: checking for positive or negative numbers
- *
  * Return: 0
  */
 
 int main(void)
-
 {
 	int i;
-
 	i = 0;
 
 	positive_or_negative(i);
-
 	return (0);
-
 }
