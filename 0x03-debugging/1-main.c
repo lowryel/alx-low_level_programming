3ac8cd1f3c202ce13616a2da15f62615fa2e72a7
lowryel <eugeneagbaglo@gmail.com>
2022-12-15 23:30:21 -0800

    a

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,14 +7,11 @@
  */
 
 int main(void)
-
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
+	printf("Infinite loop incoming :(\n");
+	i = 0;
 	/**
 	 * while (i < 10)
 	 *
@@ -22,8 +19,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
-        return (0);
+	return (0);
 }
-
