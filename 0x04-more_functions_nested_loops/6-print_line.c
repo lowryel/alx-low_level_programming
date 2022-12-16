b9cf9f0e682e8310c0e533230cbe4c175cb4b653
lowryel <eugeneagbaglo@gmail.com>
2022-12-16 03:11:15 -0800

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line - print _ for a num of input
+ *
+ * @n: input parameter
+ */
+
+void print_line(int n)
+{
+	void print_line(int n)
+
+	{
+		if (n <= 0)
+
+		{
+			_putchar('\n');
+
+		} else
+
+		{
+			int i;
+
+			for (i = 1; i <= n; i++)
+			{
+				_putchar('_');
+			}
+			_putchar('\n');
+		}
+	}
+}
